8f50aff49adbf2c7c5208963f2550218724f1563
MacApos <maciejapostol98@gmail.com>
2024-08-02 17:22:44 +0200

    initialize Elastic Beanstalk app

--- infrastructure/src/main/java/com/myorg/InfrastructureStack.java
+++ infrastructure/src/main/java/com/myorg/InfrastructureStack.java
@@ -1,14 +1,18 @@
 package com.myorg;
 
+import software.amazon.awscdk.services.elasticbeanstalk.CfnApplication;
+import software.amazon.awscdk.services.elasticbeanstalk.CfnApplicationVersion;
+import software.amazon.awscdk.services.elasticbeanstalk.CfnEnvironment;
+import software.amazon.awscdk.services.iam.CfnInstanceProfile;
+import software.amazon.awscdk.services.iam.ManagedPolicy;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.amazon.awscdk.services.s3.assets.Asset;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 
-import static java.lang.String.join;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
-import software.amazon.awscdk.services.elasticbeanstalk.*;
+import java.util.List;
 
 public class InfrastructureStack extends Stack {
     public InfrastructureStack(final Construct scope, final String id) {
@@ -24,7 +28,7 @@ public class InfrastructureStack extends Stack {
                 .build();
 
         // Create Elastic Beanstalk app
-        String appName = "NewsAppEB";
+        String appName = "NewsApp";
         CfnApplication cfnApplication = CfnApplication.Builder.create(this, appName).build();
 
         // Create Elastic Beanstalk app version
@@ -39,7 +43,32 @@ public class InfrastructureStack extends Stack {
         // Connect Elastic Beanstalk resource with S3 resource
         newsAppVersion.addDependency(cfnApplication);
 
-        
+        // Create role for ElasticBeanstalk service
+        Role role = Role.Builder.create(this, "aws-elasticbeanstalk-service-role")
+                .assumedBy(new ServicePrincipal("ec2.amazonaws.com"))
+                // ServicePrincipal.Builder.create("ec2.amazonaws.com").build())
+                .build();
 
+        // Add appropriate permission policy
+        role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AWSElasticBeanstalkWebTier"));
+
+        // Create instance profile for EC2 instances
+        String ec2RoleName = "aws-elasticbeanstalk-ec2-role";
+        CfnInstanceProfile.Builder.create(this, ec2RoleName)
+                .instanceProfileName(ec2RoleName)
+                .roles(List.of(role.getRoleName()))
+                .build();
+
+        // Create Elastic Beanstalk environment
+        String envName = appName + "Env";
+        CfnEnvironment.Builder.create(this, envName)
+                .applicationName(appName)
+                .environmentName(envName)
+                .solutionStackName("64bit Amazon Linux 2023 v5.3.0 running Tomcat 10 Corretto 17")
+                .versionLabel(newsAppVersion.getRef())
+                .build();
+
+
+//initialize
     }
 }
