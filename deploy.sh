e6d9076efb17dd1536345f1e294a2d8c4e40bb33
MacApos <maciejapostol98@gmail.com>
2024-08-26 20:06:22 +0200

    Commit 2024.08.26 20:06

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-git add .
 echo "Commit message: "
 read -r commitMessage;
 if [ -z "$varname" ]; then
     commitMessage="Commit "$(date +'%Y.%m.%d %H:%M')
 fi
+git add .
 git commit -m "$commitMessage"
 git push
 #cd software/ || exit
