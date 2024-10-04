a54821b2ea0e62062f3babbef31060182f2ed6a8
MacApos <maciejapostol98@gmail.com>
2024-10-04 02:45:34 +0200

    Lambda update

--- software/Lambda/src/main/java/com/lambda/FunctionConfiguration.java
+++ software/Lambda/src/main/java/com/lambda/FunctionConfiguration.java
@@ -1,24 +1,24 @@
 package com.lambda;
 
-import com.dataProcessingLibrary.service.ArticleService;
-import com.dataProcessingLibrary.service.FetchDataService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cglib.core.internal.Function;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = {"com.lambda", "com.dataProcessingLibrary"})
-public class FunctionConfiguration {
 
+@SpringBootApplication( scanBasePackages = {"com.lambda", "com.dataProcessingLibrary"})
+public class FunctionConfiguration {
     public static void main(String[] args) {
-        SpringApplication.run(FunctionConfiguration.class, args);
     }
 
-
     @Bean
-    public Function<String, String> uppercase(ArticleService articleService,
-                                              FetchDataService fetchDataService) {
-
-        return value -> value.toUpperCase();
+    public java.util.function.Function<String, String> uppercase() {
+        return value -> {
+            if (value.equals("exception")) {
+                throw new RuntimeException("Intentional exception");
+            }
+            else {
+                return value.toUpperCase();
+            }
+        };
     }
 }
--- software/Lambda/src/main/java/com/lambda/Handler.java
+++ software/Lambda/src/main/java/com/lambda/Handler.java
@@ -1,35 +1,67 @@
-package com.lambda;
+//package com.lambda;
+//
+//import com.amazonaws.services.lambda.runtime.Context;
+//import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+//import com.dataProcessingLibrary.dao.Article;
+//import com.dataProcessingLibrary.dao.City;
+//import com.dataProcessingLibrary.service.ArticleService;
+//import com.dataProcessingLibrary.service.DynamoDBService;
+//import com.dataProcessingLibrary.service.FetchDataService;
+//import java.io.InputStream;
+//import java.io.OutputStream;
+//import java.util.Collection;
+//import java.util.List;
+//
+//public class Handler implements RequestStreamHandler {
+//    private final FetchDataService fetchDataService;
+//    private final ArticleService articleService;
+//
+//    public static final List<City> initialCities = List.of(
+//            new City("New York", "New York"),
+//            new City("Ashburn", "Virginia"),
+//            new City("Hemingford", "Nebraska")
+//    );
+//
+//    public Handler(FetchDataService fetchDataService, ArticleService articleService) {
+//        this.fetchDataService = fetchDataService;
+//        this.articleService = articleService;
+//    }
+//
+//    @Override
+//    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) {
+//        List<Article> all = articleService.findAll();
+////        dynamoDBService.getAllNews().stream()
+////                .map(cityPage -> cityPage.items().isEmpty() ? initialCities : cityPage.items())
+////                .flatMap(Collection::stream)
+////                .forEach(city -> {
+////                    City fetch = fetchDataService.fetchNews(city).block();
+////                    if (fetch != null) {
+////                        fetch.addArticles(city.getArticles());
+////                        fetch.sortArticles();
+////                        dynamoDBService.table.putItem(fetch);
+////                    }
+////                });
+//    }
+//}
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.dataProcessingLibrary.dao.Article;
-import com.dataProcessingLibrary.dao.City;
-import com.dataProcessingLibrary.service.ArticleService;
-import com.dataProcessingLibrary.service.DynamoDBService;
-import com.dataProcessingLibrary.service.FetchDataService;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Collection;
-import java.util.List;
 
-public class Handler implements RequestStreamHandler {
-    private final FetchDataService fetchDataService;
-    private final ArticleService articleService;
-
-    public static final List<City> initialCities = List.of(
-            new City("New York", "New York"),
-            new City("Ashburn", "Virginia"),
-            new City("Hemingford", "Nebraska")
-    );
-
-    public Handler(FetchDataService fetchDataService, ArticleService articleService) {
-        this.fetchDataService = fetchDataService;
-        this.articleService = articleService;
-    }
-
-    @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) {
-        List<Article> all = articleService.findAll();
+//package com.lambda;
+//
+//import com.dataProcessLibrary.dao.City;
+//import com.dataProcessLibrary.service.DynamoDBService;
+//import com.dataProcessLibrary.service.FetchDataService;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.Collection;
+//
+//import static com.lambda.Handler.initialCities;
+//
+//class HandlerTest {
+//    private final DynamoDBService dynamoDBService = new DynamoDBService();
+//    private static final FetchDataService fetchDataService = new FetchDataService();
+//
+//    @Test
+//    void putItemsSyncTest() {
 //        dynamoDBService.getAllNews().stream()
 //                .map(cityPage -> cityPage.items().isEmpty() ? initialCities : cityPage.items())
 //                .flatMap(Collection::stream)
@@ -41,5 +73,5 @@ public class Handler implements RequestStreamHandler {
 //                        dynamoDBService.table.putItem(fetch);
 //                    }
 //                });
-    }
-}
+//    }
+//}
